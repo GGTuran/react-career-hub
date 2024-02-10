f74c1d30102a1d34e8c8710b6eeb6df1d8d1e21b
GGTuran <tayyabturan67@gmail.com>
2024-02-10 11:03:41 +0600

    styled it a bit

--- src/Components/Job/Job.jsx
+++ src/Components/Job/Job.jsx
@@ -1,11 +1,23 @@
+/* eslint-disable no-unused-vars */
 import PropTypes from 'prop-types';
 
 const Job = ({job}) => {
-    const{logo} = job;
+    const{logo, job_title, company_name, remote_or_onsite, location, job_type, salary, job_description, job_responsibility, ducational_requirements, perience } = job;
     return (
-        <div>
-            <img src={logo} alt="" />
-        </div>
+        <div className="card card-compact  bg-base-100 shadow-xl">
+  <figure><img src={logo} alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">{}job_title</h2>
+    <p>{company_name}</p>
+    <div>
+        <button className='px-5 py-2 font-extrabold border rounded border-gray-400 text-gray-900 mr-4'>{remote_or_onsite}</button>
+        <button className='px-5 py-2 font-extrabold border rounded border-gray-400  text-gray-900 mr-4'>{job_type}</button>
+    </div>
+    <div className="card-actions ">
+      <button className="btn btn-primary">View Details</button>
+    </div>
+  </div>
+</div>
     );
 };
 
