39e4c01d2da43434221ac556f39b62b6e25c2f6b
GGTuran <tayyabturan67@gmail.com>
2024-02-11 20:50:10 +0600

    storing

--- src/Components/Utility/localstorage.js
+++ src/Components/Utility/localstorage.js
@@ -1,5 +1,19 @@
+
+const getStoredJobApplication = () =>{
+    const storedJobApplication = localStorage.getItem('job-applications');
+     if(storedJobApplication){
+        return JSON.parse(storedJobApplication);
+     }
+     return[];
+}
+
+
+
+
+
+
 const saveJobApplication = id =>{
 
 }
 
-export { saveJobApplication }
+export { getStoredJobApplication , saveJobApplication }
