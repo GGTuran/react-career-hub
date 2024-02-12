1c5f9a7b2a083ad2de27df9637ee80678332eb66
GGTuran <tayyabturan67@gmail.com>
2024-02-12 20:29:39 +0600

    listing

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -32,8 +32,9 @@ const AppliedJobs = () => {
             <details className="dropdown">
                 <summary className="m-1 btn">open or close</summary>
                 <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-                <li><a>Item 1</a></li>
-                <li><a>Item 2</a></li>
+                <li><a>All</a></li>
+                <li><a>Remote</a></li>
+                <li><a>Onsite</a></li>
                 </ul>
             </details>
             <ul>
