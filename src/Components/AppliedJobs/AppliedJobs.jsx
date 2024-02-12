7614660c04af956f808ddd4ac45e2efa40930bd5
GGTuran <tayyabturan67@gmail.com>
2024-02-12 20:28:33 +0600

    ddroping

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -29,12 +29,19 @@ const AppliedJobs = () => {
     return (
         <div>
             <h2 className="text-2xl">Jobs i did apply:{appliedJobs.length}</h2>
+            <details className="dropdown">
+                <summary className="m-1 btn">open or close</summary>
+                <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                <li><a>Item 1</a></li>
+                <li><a>Item 2</a></li>
+                </ul>
+            </details>
             <ul>
                 {
 
 
                     appliedJobs.map(job => <li key={job.id}>
-                        <span>{job.job_title} for {job.company_name}</span> <span> with {job.salary}$ salary</span>
+                        <span>{job.job_title} for {job.company_name}</span> <span> with {job.salary}$ salary</span> <span>{job.remote_or_onsite}</span>
                     </li>)
 
 
